e146e6280841bf0d334da20737839041877fcf49
Juris Gribuska <juris.gribuska@bt.com>
2021-05-23 18:14:41 +0100

    almost working

--- twelve_days.rb
+++ twelve_days.rb
@@ -1,8 +1,42 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+require 'humanize'
+require "hundredth"
+require 'byebug'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
-
+class TwelveDays
+  FIRST_LINE = "day of Christmas my true love gave to me:" 
+  WORDS = [
+    "Drummers Drumming",
+    "Pipers Piping",
+    "Lords-a-Leaping",
+    "Ladies Dancing",
+    "Maids-a-Milking",
+    "Swans-a-Swimming",
+    "Geese-a-Laying",
+    "Gold Rings",
+    "Calling Birds",
+    "French Hens",
+    "Turtle Doves",
+    "a Partridge in a Pear Tree"
+  ]
+  def self.song
+    WORDS.reverse.each_with_index.inject([]) do |acc, (el, index)|
+      if (index < WORDS.size)
+        if acc[-1] == nil
+          previous_line = ""
+          acc << ("#{el}.")
+        elsif (index == 1)
+          previous_line = "and #{acc[-1]}"
+          acc << ("#{(index + 1).humanize} #{el}, #{previous_line}")
+        else
+          previous_line = acc[-1]
+          acc << ("#{(index + 1).humanize} #{el}, #{previous_line}")
+        end
+        # acc << (previous_line + "On the #{Hundredth.ordinal(index + 1)} #{FIRST_LINE} #{(index + 1).humanize} #{el}.")
+        acc
+      end
+    end.each_with_index.inject([]) do |acc, (el, index)|
+      acc << "On the #{Hundredth.ordinal(index + 1)} #{FIRST_LINE} #{el}"
+      acc
+    end.join("\n\n")
+  end
+end
--- twelve_days_test.rb
+++ twelve_days_test.rb
@@ -5,6 +5,7 @@ class TwelveDaysTest < Minitest::Test
   def test_the_whole_song
     song_file = File.expand_path('../song.txt', __FILE__)
     expected = IO.read(song_file)
+    debugger
     assert_equal expected, TwelveDays.song
   end
 end
