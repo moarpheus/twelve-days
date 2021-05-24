df5c53883be8c69d895eddb19faf96a10c0a4301
Juris Gribuska <juris.gribuska@bt.com>
2021-05-24 06:20:22 +0100

    little restyling

--- twelve_days.rb
+++ twelve_days.rb
@@ -3,6 +3,7 @@ require "hundredth"
 
 class TwelveDays
   FIRST_LINE = "day of Christmas my true love gave to me:" 
+
   WORDS = [
     "Drummers Drumming",
     "Pipers Piping",
@@ -17,19 +18,20 @@ class TwelveDays
     "Turtle Doves",
     "a Partridge in a Pear Tree"
   ]
+
   def self.song
     output = []
     WORDS.reverse.each_with_index.inject([]) do |acc, (el, index)|
-      if (index < WORDS.size)
-        if acc[-1] == nil
+      if index < WORDS.size
+        if acc.last.nil?
           previous_line = ""
-          acc << ("#{el}.\n")
-        elsif (index == 1)
-          previous_line = "and #{acc[-1]}"
-          acc << ("#{(index + 1).humanize} #{el}, #{previous_line}")
+          acc << "#{el}.\n"
+        elsif index == 1
+          previous_line = "and #{acc.last}"
+          acc << "#{(index + 1).humanize} #{el}, #{previous_line}"
         else
-          previous_line = acc[-1]
-          acc << ("#{(index + 1).humanize} #{el}, #{previous_line}")
+          previous_line = acc.last
+          acc << "#{(index + 1).humanize} #{el}, #{previous_line}"
         end
         output << "On the #{Hundredth.ordinal(index + 1)} #{FIRST_LINE} #{acc.last}"
         acc
